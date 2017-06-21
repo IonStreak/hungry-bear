5c9956af9c44a31527d2878cf0fdedb4831c0a81
IonStreak <christopherl120@eagles.hba.net>
2017-06-20 14:57:46 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log (humanNames);
+	console.log("bearName ate " + humanNames[2])
+}
+
+eatHumans("Me", ["Dude", "The Dude", "El Duderino"]);
+
